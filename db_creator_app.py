e6bd736a164aff223289d7973675812e9b861189
ronaks2309 <36353981+ronaks2309@users.noreply.github.com>
2024-01-28 18:22:01 -0800

    Update db_creator_app.py

--- db_creator_app.py
+++ db_creator_app.py
@@ -21,7 +21,7 @@ def create_vector_index_from_pdf(uploaded_files,password):
         st.warning("Incorrect Password")
         return
     if check_index() != "Valid":
-        st.warning("Index Name Not Valid")
+        st.warning("Agent Name Not Valid")
         return
     data_folder = save_files(uploaded_files)
     text_chunks = get_chunks(data_folder)
@@ -61,35 +61,39 @@ def check_index():
     pinecone.init(environment='gcp-starter')
     index_name = st.session_state.new_index
     if index_name == "":
-        st.warning('Index name cannot be blank', icon="⚠️")
+        st.warning('Agent name cannot be blank', icon="⚠️")
         return "Invalid"
     elif index_name in pinecone.list_indexes():
-        st.warning('Index already exists', icon="⚠️")
+        st.warning('Agent already exists', icon="⚠️")
         return "Invalid"
     else: 
-        st.success('Index name available :white_check_mark:')
+        st.success('Agent name available :white_check_mark:')
         return "Valid"
 
 def main():
     print("Main method called")
     load_dotenv()
-    st.set_page_config(page_title="DB Creator", page_icon = ":books:")
+    st.set_page_config(page_title="Custom Agents Builder", page_icon = ":books:")
     sideb = st.sidebar
     #st.title("Create a vector index from knowledge files :database:")
-    st.subheader("Load PDF files into a new Pinecone Index")
+    st.subheader("Build your Agent with Domain Specific Knowledge")
     
     col1, col2 = st.columns(2)
-    col1.text_input("New Index Name", label_visibility='collapsed', placeholder="Enter name for New Pinecone Index", key='new_index')
+    col1.text_input("Agent Name", label_visibility='collapsed', placeholder="Enter name for New Agent", key='new_index')
     col2.button("Check", on_click= check_index)
     
-    uploaded_files = st.file_uploader("Upload files", accept_multiple_files=True)
+    st.text_area("Agent Description", placeholder="Enter a short description of your agent")
+    
+    st.text_area("Agent Instructions",placeholder="Enter Special instructions that you want the agent to follow.\n For ex. Always be courteous. You replies should only be based on the domain knowledge provided. If you don't know the answer, guide the user to speak with a WMC Associate. Do not give financial advice.")
+
+    uploaded_files = st.file_uploader("Upload Domain Knowledge files", accept_multiple_files=True)
     
     st.text_input("Enter Password", type = "password", placeholder="Enter Password", key='password')    
     
     if st.button("Create"):
         with st.spinner("Processing"):
             pinecone_index = create_vector_index_from_pdf(uploaded_files,st.session_state.password)
-            st.success("Pinecone Index created successfully !!")
+            st.success("Agent created successfully !!")
     
     
 
